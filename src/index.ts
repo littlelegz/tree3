45d3f9d603568c2a494fc6b6c44615644d87f1a3
littlelegz <zhaoj16@ncbi.nlm.nih.gov>
2025-02-13 16:59:25 -0500

    export utils

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ export { default as UnrootedTree } from './components/unrooted';
 // Types
 export * from './components/types';
 
+export * from './components/utils';
+
 export function add(a: number, b: number): number {
     return a + b;
 }
